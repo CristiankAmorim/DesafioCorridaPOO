d1d464ff61e6989e31d5e2d0a3d60825e7bca4ce
CristiankAmorim <camorim1100@gmail.com>
2025-04-28 16:46:55 -0300

    Update Pista.java
    Eliminei as duas condicionais if que limitavam as distâncias percorridas pelos carros. Caso a distância de algum carro, ao final da corrida, fosse maior que o tamanho da pista, esta, seria alterada e receberia o valor do tamanho da pista. O enunciado do trabalho deixa claro que o carro pode percorrer uma distância maior do que a pista; e ainda diz que, caso ambos os carros excedam esse limite, vence aquele que percorreu uma distância maior.

--- src/Pista.java
+++ src/Pista.java
@@ -19,56 +19,53 @@ public class Pista {
     Carro carroUm = new Carro("Carro Um");
     Carro carroDois = new Carro("carro Dois");
 
-    public void visual() {
+   public void visual() {
 
-    System.out.println("INICIANDO CORRIDA DE ASTERISCO");
-    System.out.println();
+    	System.out.println("INICIANDO CORRIDA DE ASTERISCO");
+    	System.out.println();
 
-    String visualDaPista = "";
-    for (int i = 0; i < this.tamanhoDaPista; i++) {
-        visualDaPista += "-";
-    }
-     System.out.println(visualDaPista);
-     System.out.println("#");
-     System.out.println("#");
-     System.out.println(visualDaPista);
-     System.out.println();
+    	String visualDaPista = "";
+    	for (int i = 0; i < this.tamanhoDaPista; i++) {
+    		visualDaPista += "-";
+    	}
+    	System.out.println(visualDaPista);
+    	System.out.println("#");
+    	System.out.println("#");
+    	System.out.println(visualDaPista);
+    	System.out.println();
 
-    do{
-    	carroUm.corridaIniciar();
-        carroDois.corridaIniciar();
+    	do{
+    		carroUm.corridaIniciar();
+    		carroDois.corridaIniciar();
 
-         if(carroUm.getDistancia() > this.tamanhoDaPista){
-            carroUm.setDistancia(this.tamanhoDaPista);
-         }
-            
-         if(carroDois.getDistancia() > this.tamanhoDaPista){
-            carroDois.setDistancia(this.tamanhoDaPista);
-         }
+    		System.out.println(visualDaPista);
+    		for(int i = 0; i < carroUm.getDistancia(); i++){
+    			System.out.print(" ");
+    		}
+    		System.out.println("#");
 
-         System.out.println(visualDaPista);
-         for(int i = 0; i < carroUm.getDistancia(); i++){
-        	 System.out.print(" ");
-         }
-         System.out.println("#");
-
-         for(int i = 0; i < carroDois.getDistancia(); i++){
-        	 System.out.print(" ");
-         }
-         System.out.println("#");
+    		for(int i = 0; i < carroDois.getDistancia(); i++){
+    			System.out.print(" ");
+    		}	
+    		System.out.println("#");
        
-         System.out.println(visualDaPista);
-         System.out.println();
-
-     }while (carroUm.getDistancia() < this.tamanhoDaPista && carroDois.getDistancia() < this.tamanhoDaPista);
-
+    		System.out.println(visualDaPista);
+    		System.out.println();
+         
+    	}while (carroUm.getDistancia() < this.tamanhoDaPista && carroDois.getDistancia() < this.tamanhoDaPista);
+    	
+    	System.out.println("Distancias Percorridas");
+    	System.out.println(carroUm.getNomeDoCarro() + ": " + carroUm.getDistancia());
+    	System.out.println(carroDois.getNomeDoCarro()+ ": " + carroDois.getDistancia());
+    
      	if(carroUm.getDistancia()  > carroDois.getDistancia()){
             carroUm.vencedor();
         }else if(carroDois.getDistancia() > carroUm.getDistancia()){
             carroDois.vencedor();
         }else {
         	System.out.println("EMPATE!!!");
-        }	  
+        }	
+        
      }
 
 }
