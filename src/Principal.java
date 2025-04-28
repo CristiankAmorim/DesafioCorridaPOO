75ad4a2d93bf202b44d3a43c83098001b5e3853d
CristiankAmorim <camorim1100@gmail.com>
2025-04-28 15:05:34 -0300

    Update Principal.java
    Nesta classe, onde se encontra o método main, as únicas alterações foram: Durante a instância do objeto pista incluir um arguemento, que é o tamanho da pista (do tipo int); e chamar o método visual(), que antes se chamava frontend().

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,8 @@
 public class Principal {
     public static void main(String[] args) {
 
-        Pista pista = new Pista();
-
-            pista.frontEnd();
-
+        Pista pista = new Pista(30);
+        
+        pista.visual();
     }
 }
